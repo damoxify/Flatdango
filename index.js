32d9059786fe05129f14a20fd4f577009701409b
Adedamola Ajasa <azeemajasa@gmail.com>
2023-08-25 23:19:13 +0100

    get request to see film details

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+const main = document.querySelector("main")
 document.addEventListener("DOMContentLoaded", ()=> {
-    fetch("http://localhost:3000/films")
+    return fetch("http://localhost:3000/films/2")
     .then((res)=> res.json())
-    .then((data)=> console.log(data))
+    .then((data)=> {
+        const poster = document.createElement("img")
+        poster.src = data.poster
+        main.appendChild(poster)
+        const h2 = document.createElement("h2")
+        h2.innerHTML = `Title: ${data.title}`
+        main.appendChild(h2);
+        const firstParagraph = document.createElement("p")
+        firstParagraph.textContent = `Runtime: ${data.runtime} ` 
+        main.appendChild(firstParagraph)
+        const secondParagraph = document.createElement("p")
+        secondParagraph.textContent = `Showtime: ${data.showtime}`
+        main.appendChild(secondParagraph)
+        const capacity =  data.capacity
+        const ticketSold = data.tickets_sold
+        console.log(data)
+        const paragraph = document.createElement("p")
+        paragraph.textContent = `Available tickets: ${capacity - ticketSold}`
+        main.appendChild(paragraph) 
+    })
 })
+
